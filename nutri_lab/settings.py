6cfa619cf158275c02fad0ab39488e83df516145
darkpwd <natanyuri1010@gmail.com>
2024-09-02 12:18:15 -0300

    Atualizações no settings.py, atualizei o debug

--- nutri_lab/settings.py
+++ nutri_lab/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ahxyl=*(k2w6u-h$z@zi=qj&px1$yx&su@$_$lr!atv6mophh9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS= (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('staticfiles')
+STATIC_ROOT = os.path.join('static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 
